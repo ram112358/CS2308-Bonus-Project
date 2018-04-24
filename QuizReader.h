87c9b7d7bb93da7f647c1fa405479e44456410b8
legomaniack <ng31415@gmail.com>
2018-04-23 21:38:06 -0500

    Implemented readFile parse function. It compiles now I guess

--- QuizReader.h
+++ QuizReader.h
@@ -3,12 +3,13 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 struct Question {
     std::string title;
     std::vector<std::string> answers;
     int correct;
-}
+};
 
 class QuizReader {
 private:
@@ -23,11 +24,38 @@ public:
     std::string getTitle() {
         return title;
     }
-}
+};
 
 // Takes a quiz file and parses it into a vector of questions
-QuizReader::readFile(std::string filename) {
-    
+void QuizReader::readFile(std::string filename) {
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Error opening file" << '\n';
+    }
+    std::string currLine;
+    std::getline(file, currLine);
+    title = currLine;
+
+    while (std::getline(file, currLine)) {
+        Question q* = new Question;
+        q->title = currLine;
+        std::getline(file, currLine);
+
+        std::stringstream ss(currLine);
+        std::string answer;
+        std::vector<std::string> tokens;
+        while (getline(ss, answer, "/")) {
+            tokens.push_back(answer);
+        }
+        q->answers = tokens;
+
+        for (int i=0; i < q->answers.size(); i++) {
+            if q->answers[i][0] == '~';
+            q->answers[i].substr(1, std::string::npos);
+            q->correct = i;
+        }
+
+    }
 }
 
 #endif
