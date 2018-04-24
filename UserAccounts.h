1edd37206330c875807f2230ec42a9c1a655e753
Ram Moore <rammoore112358@gmail.com>
2018-04-24 14:43:09 -0500

    added loadAccountData

--- UserAccounts.h
+++ UserAccounts.h
@@ -3,20 +3,24 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 struct Account {
     string Username;
-    vector<int> quizzes taken;
+    vector<int> quizHist;
     int grade;
 }
 
 class UserAccounts {
 private:
     vector<Account> accounts;
+    vecctor<string> fileData;
     Account current;
-    fstream file;
+    fstream accountFile("Account.Data", ios::in|ios:: out);
+    void loadAccountData(); // load the accounts from the file to the accounts vector
+    void storeAccountData(); // stores the accounts vector into the file
 public:
     UserAccounts(){
         if (!file) {
@@ -24,11 +28,8 @@ public:
         }
     }
 
-    void logIn();
-    void loadAccountData(); // load the accounts from the file to the accounts vector
-    void storeAccountData(); // stores the accounts vector into the file
+    void logIn(); //
     void enterGrade(int quizNumber, int grade); // enter a grade for a specific quiz out of 100
-
 
     void initializeAccount(); // add an account to the file
     void setUpAccount(string username, string password); // prompt the user for a username and password and then call inidializeAccount
@@ -37,5 +38,16 @@ public:
 }
 
 void UserAccounts::loadAccountData(){
-
+    string temp;
+    int tempInt;
+    while(getline(accountFile, line)){fileData.push_back(line)}
+    for(i = 0; i < fileData.size(); i+=3){
+        accounts[i/3].Username = fileData[i];
+        stringstream ss(fileData[i+2]);
+        while(getline(ss, )){
+            stringstream int(temp);
+            temp >> tempInt;
+            accounts[i/3].quizHist.push_back(tempInt);
+        }
+    }
 }
